1fcde63620a411892a16b260924776dec4e93fc2
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-07-24 16:29:22 +0530

    prechanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { MyProfile2Component } from './private-pages/dashboard/my-profile2/my-profile2.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ProfileHeaderComponent } from './common/profile-header/profile-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MyProfile2Component, ProfileHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
