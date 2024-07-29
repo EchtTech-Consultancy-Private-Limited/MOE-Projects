7a790472aa2dcaed0fa7a47b0e863f8dda4acd65
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-07-29 19:42:25 +0530

    fixed ui issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,39 @@
+// export const environment = {
+//   production: false,
+//   staging: false,
+//   baseUrl: 'http://localhost:4200',
+//   nspApiUrl: 'http://10.247.138.26:8082/NspApiFreshdev/',
+//   nspApiUrlFresh: 'http://10.247.138.26:8082/NspApiFreshdev/',
+//   // nspApiUrl: 'http://10.247.138.26:8082/NspApiFresh/',
+//   nspRegUrl: 'http://10.247.138.26/otrappstaging/',
+//   // http://10.247.138.26:8082/NspApiFresh/nsp/saveAdditionalData
+//   nspUrl: 'http://10.247.138.26/',
+//   topsApiURL: '',
+//   nsrsApiURL: '', //for local
+//   fileUrl: '',
+//   downloadExcel: '',
+//   ssoLoginUrl: '',
+//   secretPasswordKey: 'Eth@n01',
+//   //Keys for Encrypt and Decrypt
+//   enc_Key: '7739826323491690',
+//   ency_iv: '7739826323491690',
+
+//   //encryption app id
+//   encrAppId: 'RVq6Hi700P',
+// };
+
 export const environment = {
-  production: false,
-  staging: false,
-  baseUrl:
-    "http://2605-2402-e280-4117-34-e11a-deac-2061-ac2e.ngrok-free.app/users",
-  newBaseUrl:
-    "http://2605-2402-e280-4117-34-e11a-deac-2061-ac2e.ngrok-free.app/",
+  production: true,
+
+  baseUrl: 'https://scholarships.gov.in/ApplicationForm/',
+  nspApiUrl: 'https://scholarships.gov.in/NspApiFresh/',
+  nspApiUrlFresh: 'https://scholarships.gov.in/NspApiFresh/',
+  nspUrl: 'https://scholarships.gov.in/',
+  //for local
+  fileUrl: '',
+  downloadExcel: '',
+  ssoLoginUrl: '',
+  enc_Key: '7739826323491690',
+  ency_iv: '7739826323491690',
+  encrAppId: 'RVq6Hi700P',
 };
