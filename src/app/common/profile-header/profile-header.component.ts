8e5bd04ca09f42e3f2c7ac86683b7062bdbbe4e4
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-07-31 14:47:06 +0530

    fixed changes

--- src/app/common/profile-header/profile-header.component.ts
+++ src/app/common/profile-header/profile-header.component.ts
@@ -5,10 +5,12 @@ import { MatIconModule } from "@angular/material/icon"; // Import MatIconModule
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { Router } from "@angular/router";
 import { AlertServiceService } from "../aleart.service";
+import { RouterLink } from "@angular/router";
+
 @Component({
   selector: "app-profile-header",
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule, MatToolbarModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatToolbarModule, RouterLink],
   templateUrl: "./profile-header.component.html",
   styleUrls: ["./profile-header.component.scss"],
 })
